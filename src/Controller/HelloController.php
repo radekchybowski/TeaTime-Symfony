95d75447b81f85c78ccf729ebc6cf57eed98d85d
Radek <radek@MacBook-Air-Radek.local>
2023-05-23 21:45:18 +0200

    first commit

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Hello controller.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ *Class HelloController.
+ */
+
+#[Route('/hello')]
+class HelloController extends AbstractController
+{
+    #[Route(
+        '/{name}',
+        name: 'hello_index',
+        requirements: ['name' => '[a-zA-Z]+'],
+        defaults: ['name' => 'World'],
+        methods: 'GET'
+    )]
+    public function index(string $name): Response
+    {
+        return $this->render(
+            'hello/index.html.twig',
+            ['name' => $name]
+        );
+    }
+}
