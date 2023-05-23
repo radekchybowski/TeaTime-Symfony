7e5633b7d78c277ab5366a9423cb6a5856954ca4
Radek <radek@MacBook-Air-Radek.local>
2023-05-24 01:18:01 +0200

    second commit

---
+++ src/Controller/RecordController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RecordRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RecordController extends AbstractController
+{
+    #[Route('/record', name: 'app_record')]
+    public function index(RecordRepository $repository): Response
+    {
+        $records = $repository->findAll();
+
+        return $this->render(
+            'record/index.html.twig',
+            [
+                'controller_name' => 'RecordController',
+                'records' => $records
+        ]);
+    }
+
+    #[Route(
+        '/{id}',
+        name: 'record_show',
+        requirements: ['id' => '[1-9]\d*'],
+        methods: 'GET'
+    )]
+    public function show(RecordRepository $repo, int $id) : Response
+    {
+        $record = $repo->findOneById($id);
+
+        return $this->render(
+            'record/show.html.twig',
+            ['record' => $record]
+        );
+    }
+}
